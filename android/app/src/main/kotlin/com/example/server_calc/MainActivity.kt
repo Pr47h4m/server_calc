0ffa091f0ada90805803ec328d404ae6f016bf0e
pr47h4m <pr47h4m@gmail.com>
2021-03-12 16:26:43 +0530

    Initialised project.
    Created blank flutter firebase project

---
+++ android/app/src/main/kotlin/com/example/server_calc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.server_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ServerCalc());
+
+class ServerCalc extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
