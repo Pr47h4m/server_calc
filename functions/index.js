efca2822288af998b4c8ad7a41354694644957e0
pr47h4m <pr47h4m@gmail.com>
2021-03-12 17:21:08 +0530

    Created Functin
    Created Function To Calculate, Store, Return Data

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,47 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const database = admin.database();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/calculate', (req, res, next) => {
+    const body = req.body;
+    const num1 = body.num1;
+    const num2 = body.num2;
+    const op = body.op;
+    let result;
+    if (op === "+") {
+        result = num1 + num2;
+    } else if (op === "-") {
+        result = num1 - num2;
+    } else if (op === "*") {
+        result = num1 * num2;
+    } else if (op === "/") {
+        result = num1 / num2;
+    }
+    let response = {
+        num1: num1,
+        num2: num2,
+        op: op,
+        result: result
+    };
+    try {
+        var timestamp = new Date();
+        database.ref('history').push(
+            response
+        );
+    } catch (err) {
+        console.log(error);
+    }
+    return res.send(response);
+});
+
+exports.webApi = functions.https.onRequest(app);
